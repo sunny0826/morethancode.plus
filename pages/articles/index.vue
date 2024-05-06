ebf51246187142023f883daf5a7a35c45a4ac4d9
Xudong Guo <guoxudong.dev@gmail.com>
2024-05-06 10:01:06 +0800

    fix deploy

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -11,7 +11,7 @@
 
 <script setup>
 const description =
-  "All of my long-form thoughts on programming, user interfaces, product design, and more, collected in chronological order.";
+  "All of my long-form thoughts on programming, open source, product design, and more, collected in chronological order.";
 useSeoMeta({
   title: "Articles | Xudong Guo",
   description,
